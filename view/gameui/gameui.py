276a5e6542f1c02b9f7cf09807d7ac0f53a91f95
ciarancotter <120326406@umail.ucc.ie>
2023-01-25 19:26:27 +0000

    Added docstrings to the text box utility.

--- view/gameui/gameui.py
+++ view/gameui/gameui.py
@@ -1,15 +1,43 @@
+"""A utility to create a text box at the bottom of the screen.
+
+This class can be used to create a text box with custom text in the Pygame UI.
+
+Usage:
+
+    myTextBox = UITextBox(screen, 20, "monospace", 14)
+    myTextBox.draw("Pyramids look funny.")
+    myTextBox.erase()
+"""
+
 import pygame
 
 
 class UITextBox:
+    """A UI text box to write a piece of dialogue or information at the bottom of the screen.
+
+    A UI text box that scales quite well.
+
+    Attributes:
+        screen: The pygame screen.
+        margin: The space between the window and the box at either side of it.
+        font: The font used in the text box.
+        fontsize: The size of the text.
+    """
 
     def __init__(self, screen, margin, font, fontsize):
+        """Inits UITextBox
+        """
         self.screen = screen
         self.margin = margin
         self.font = font
         self.fontsize = fontsize
 
     def draw(self, text: str):
+        """Draws the box to the screen.
+        
+        Args:
+            text: The text to write in the box.
+        """
         dimensions = self.screen.get_size()[0]
         box = pygame.Surface((dimensions - self.margin, dimensions // 8))
         box.fill("white")
@@ -21,6 +49,8 @@ class UITextBox:
         self.screen.blit(text, text_rect)
 
     def erase(self):
+        """Removes the current text.
+        """
         dimensions = self.screen.get_size()[0]
         box = pygame.Surface((dimensions - self.margin, dimensions // 8))
         box.fill("white")
