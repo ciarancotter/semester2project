689e246fd40c0bdeefc81578967222bbc85e3175
Shaza Aldawamneh <118107805@umail.ucc.ie>
2023-01-27 18:33:58 +0000

    added docstring

--- src/tester.py
+++ src/tester.py
@@ -7,6 +7,7 @@ from pygame.locals import (
     K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
+    K_SPACE,
     QUIT,
 )
 
@@ -17,41 +18,63 @@ SCREEN_HEIGHT = 600
 
 #main character class
 class Player(pygame.sprite.Sprite):
-
+    """
+        The Player Class is used to initilize the player and it's movements
+        Attributes:
+        - playerWidth: width of player image
+        - playerHeight: hight of player image
+        - playerxChange:  the x axes change for the player
+        - playeryChange: the y axes change for the player
+        - playerX: position of player in x axis
+        - playerY: position of player in  y axis
+        - playerImage: image of player
+        Methods:
+        - update(pressed_keys):  get the pressed key event and it apply it by moving right,left,jump or no action required 
+    """
     def __init__(self):
-        super(Player, self).__init__()
-        self.x = SCREEN_WIDTH / 2  #x co-ords for start position
-        self.y = SCREEN_HEIGHT / 2  #y co-ords for start position
-        #just for now its a black box
-        self.surf = pygame.Surface((75, 50))
-        self.surf.fill((0, 0, 0))
-        #initial position
-        self.rect = self.surf.get_rect(center=(self.x, self.y))
+        self.animationList = []
+        self.animationStep = 4
+        #self.neg = 0
+        self.playerwidth = 50
+        self.playerHeight = 75
+        self.playerframeL = False
+        self.playerframeR = False
+        self.playerxChange = 0
+        self.playeryChange = 0
+        # self.vel= 5 #velosity, how fast char move 
+        self.playerX = SCREEN_WIDTH / 2  #x co-ords for start position
+        self.playerY = SCREEN_HEIGHT / 2  #y co-ords for start position
 
-    def gravity(self):
-        self.rect.move_ip(0, 1)
+        self.playerImage = "frame.png"
+
 
     # Move the sprite based on user keypresses
     def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        #might not be needed
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+        """
+        it get called when user press any keys of the 3 specified, Left Arrow, Right Arrow and Space 
+        """
+        #for vector movement x
+        self.playerX = self.playerX + self.playerxChange
+        self.playerxChange = self.playerxChange *0.10
+        #for vector movement y
+        self.playerY = self.playerY + self.playeryChange
+        self.playeryChange = self.playeryChange *0.10
 
-        # Keep player on the screen
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+        if pressed_keys[K_LEFT] and self.playerX> 0:
+            self.playerxChange = self.playerxChange - 0.2
+            #self.playerX -= self.vel
+        if pressed_keys[K_RIGHT] and self.playerX < SCREEN_WIDTH - self.playerwidth :
+            self.playerxChange = self.playerxChange + 0.2
+
+        if pressed_keys[K_SPACE] and self.playerY  > 0 :
+            self.playeryChange = self.playeryChange - 4
+            #self.playerY -= self.vel
+
+        if self.playerY < SCREEN_HEIGHT - self.playerHeight :
+            self.playerY += 2
+  
+
+
 
 
 # Initialize pygame
@@ -60,16 +83,17 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Instantiate player.(just black box rightnow )
-player = Player()
 
 # Keep main loop running
 running = True
-
+player = Player()
 # Main loop
 while running:
-    player.gravity()
-
+    """
+    the game run while state of running is true,
+    game can be stopped if user Quit using the exit buttom or exit through the Escape key
+    """
+    #player.gravity()
     for event in pygame.event.get():
         # Check if key pressed (KEYDOWN event)
         if event.type == KEYDOWN:
@@ -79,18 +103,18 @@ while running:
         # Check for QUIT event. If QUIT, then set running to false.
         elif event.type == QUIT:
             running = False
-
-    # Get the set of keys pressed and check for user input
+   
+        
     pressed_keys = pygame.key.get_pressed()
-
-    # Update the player sprite based on user keypresses
     player.update(pressed_keys)
 
-    # Fill the screen with white
-    screen.fill((255, 255, 255))
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (255,0,0), (player.playerX, player.playerY, player.playerwidth, player.playerHeight))
+    # Get the set of keys pressed and check for user input
+    pygame.display.update()
 
-    # Draw the player on the screen
-    screen.blit(player.surf, player.rect)
+
+
 
     # Update the display
     pygame.display.flip()
