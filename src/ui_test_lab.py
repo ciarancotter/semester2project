4798214e2fdd6fb5c5690fe588f9a3d4dc7b555d
ciarancotter <120326406@umail.ucc.ie>
2023-02-01 12:30:49 +0000

    Updated the README file to be less like the design document.

---
+++ src/ui_test_lab.py
@@ -0,0 +1,58 @@
+import pygame
+from view.gameui import gameui, healthbar
+from model.aiutilities import aiutilities
+
+# Initialize pygame and create a window
+pygame.init()
+dimensionX = 1280
+dimensionY = 768
+#aiutilities.configure_openai()
+#legend = aiutilities.generate_monolith("tragic", "roman")
+
+screen = pygame.display.set_mode((dimensionX, dimensionY))
+# Text Box
+#myTextBox = gameui.UITextBox(screen, 30, "monospace", 12)
+#myTextBox.draw("placeholder")
+
+# Health Bar
+#myHealthBar = healthbar.HealthBar(100)
+#myHealthBar.drawMaxHealth(screen)
+#myHealthBar.drawCurrentHealth(screen, 100)
+
+# Mini Window
+myMiniWindow = gameui.GameWindow(screen)
+myMiniWindow.draw()
+pygame.display.update()
+
+# Main game loop
+running = True
+spacePressed = False
+currentLine = 0
+
+while running:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and spacePressed == False:
+                if currentLine < len(legend):
+                    #myTextBox.erase()
+                    #myTextBox.draw(legend[currentLine])
+                    spacePressed = True
+                    currentLine += 1
+                    #myHealthBar.drawCurrentHealth(screen,
+                     #                             (100 - 10 * currentLine))
+                else:
+                    #myTextBox.erase()
+                    #myTextBox.draw("End.")
+                    break
+            pygame.display.update()
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                spacePressed = False
+
+# Exit pygame
+pygame.quit()
