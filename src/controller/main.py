3d6993578f3c29cacfe9cc170648d3fa6bacd816
Bradley Harris <bradx135@gmail.com>
2023-02-13 13:01:47 +0000

    reduce main

--- src/controller/main.py
+++ src/controller/main.py
@@ -3,11 +3,9 @@ import sys
 import os
 
 sys.path.append(os.path.abspath("./src"))
-#from model.gameobjects.game_interface import *
-#from view.gameui.scene import *
+from model.gameobjects.game_interface import PlatformerGame
+from view.gameui.scene import Scene
 
-# Initialize pygame
-pygame.init()
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
@@ -36,43 +34,15 @@ from pygame.locals import (
 
 def main():
     # Initialize pygame
-    #pygame.init()
-
-    # Set screen size and title for main menu
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # loading the game panel for main menu and game
-    '''-----commented out until for the moment --------
-    game = PlatformerGame()
-    ctx = game.get_render_ctx()
-    for block in ctx.blocks:
-        print(block.coordinates)
-    myScene = Scene(game)
-    myScene.initialiseMenuScene()   #not sure if this should go in controller
-    myScene.initialiseGameScene()   #not sure if this should go in controller
-    myScene.updateScene()
-    ----------------------'''
-
-    # puts the text part of the display in
-    """myElementWindow = ElementWindow(screen)
-    myElementWindow.draw(myGameWindow)"""
-
-    # puts the kinect part of the display in
-    """myKineectWindow = ElementWindow(screen)
-    myKinectWindow.draw(myGameWindow)"""
-
-    # refresh entire screen
-    pygame.display.flip()
+    pygame.init()
 
     # a boolean to ensure the game is running
     running = True
 
-    # get the player class from pygame_tester(needs to be changed)
-    # mainPlayer = pygame_tester.Player()
-    # get the box class from pygame_tester
-    # blueBox = pygame_tester.Box()
-
     clock = pygame.time.Clock()
+
+    gamemanager = PlatformerGame()
+    gamepanel = Scene(gamemanager)
 
     # Main game loop
     while running:
@@ -87,22 +57,22 @@ def main():
 
         # <-- Update calls go here -->
         pressed_keys = pygame.key.get_pressed()
-        # mainPlayer.update(pressed_keys)
-        # pygame_tester.collision_with_obj(mainPlayer, blueBox)
+
+        # loading the game panel for main menu and game
+        gamepanel.updateScene()
+
+        
 
         # <-- View calls go here -->
         # game screen needs to be drawn after update call
-        # myGameWindow.draw()
-        # GamePanel.draw()
-        # screen.blit(mainPlayer.image, mainPlayer.rect)
-        # screen.blit(blueBox.image, blueBox.rect)
+        
 
         # refresh entire screen
         pygame.display.flip()
 
     # Exit pygame
     pygame.quit()
-    # sys.exit()
+    sys.exit()
 
 
 if __name__ == "__main__":
