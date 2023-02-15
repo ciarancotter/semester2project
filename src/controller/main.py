b8836fe7f264db12bec89ac039fe4e7a49ad2c64
niamhConnolly1 <120346431@umail.ucc.ie>
2023-02-15 13:44:49 +0000

    fixed the movement

--- src/controller/main.py
+++ src/controller/main.py
@@ -50,8 +50,15 @@ def main() -> None:
 
 
         keys_pressed = pygame.key.get_pressed()
-        
-        if keys_pressed[]
+
+        if keys_pressed[K_LEFT]:
+            gamemanager.update_model(Movement.left)
+        elif keys_pressed[K_RIGHT]:
+            gamemanager.update_model(Movement.right)
+        elif keys_pressed[K_SPACE]:
+            gamemanager.update_model(Movement.jump)
+        else:
+            gamemanager.update_model(Movement.no_movement)
 
             
 
