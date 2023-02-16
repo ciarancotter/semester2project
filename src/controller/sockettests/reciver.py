27ea52c48d7fb081a824447adb830b787e418938
Bradley Harris <47424953+BradleyHarris19@users.noreply.github.com>
2023-02-16 16:23:52 +0000

    more socket tests

--- src/controller/sockettests/reciver.py
+++ src/controller/sockettests/reciver.py
@@ -1,15 +1,26 @@
-import socket
+import asyncio
+import websockets
 import json
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+async def receive(websocket):
     while True:
-        data = s.recv(22)
-        if not data:
-            break
-        json_data = json.loads(data.decode('utf-8'))
-        print("Received:", json_data)
+        try:
+            message = await websocket.recv()
+            data = json.loads(message)
+            print(data)
+        except:
+            pass
 
+async def corun():
+    print("run")
+
+async def main():
+    async with websockets.connect("ws://127.0.0.1:65432") as websocket:
+        await asyncio.gather(receive(websocket), corun())
+        await asyncio.Future()
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- src/controller/sockettests/sender.py
+++ src/controller/sockettests/sender.py
@@ -1,6 +1,7 @@
-import socket
+import asyncio
+import websockets
 import json
-import sys
+import time
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -8,13 +9,12 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 data = {"HandInfront": False}
 json_data = json.dumps(data).encode('utf-8')
 
-print(len(json_data))
+async def send(websocket, path):
+        await websocket.send(json_data)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        while True:
-            print(f"Sent {data}")
-            conn.sendall(json_data)
+async def main():
+    async with websockets.serve(send, "127.0.0.1", 65432):
+        await asyncio.Future()  # Run forever
+
+if __name__ == '__main__':
+    asyncio.run(main())
