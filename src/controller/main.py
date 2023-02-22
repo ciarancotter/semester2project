5732f99daa8520946563d113423172d5b6cf8dc6
ciarancotter <120326406@umail.ucc.ie>
2023-02-22 22:59:06 +0000

    Fixed main.py

---
+++ src/controller/main.py
@@ -0,0 +1,24 @@
+from multiprocessing import Process
+from game import main
+
+
+def run_kinect():
+    try:
+        from kinect import MovementHandler
+        mv = MovementHandler(100, 100)
+        while True:
+            mv.update()
+    except Exception as e:
+        print(e)
+        return
+
+
+def run_game():
+    main()
+
+
+if __name__ == '__main__':
+    p1 = Process(target=run_kinect)
+    p2 = Process(target=run_game)
+    p1.start()
+    p2.start()
