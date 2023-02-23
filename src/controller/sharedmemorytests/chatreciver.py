8b15093cae1612333a02c19c1dbb7089bf7e678f
Bradley Harris <47424953+BradleyHarris19@users.noreply.github.com>
2023-02-23 13:59:38 +0000

    first send

---
+++ src/controller/sharedmemorytests/chatreciver.py
@@ -0,0 +1,29 @@
+import pygame
+import numpy as np
+from shared_memory_dict import SharedMemoryDict
+
+# Create a numpy array
+video = SharedMemoryDict(name='movementVideo', size=1000000)
+
+print(type(video["src"]))
+# Convert the numpy array to a pygame surface
+surface = pygame.surfarray.make_surface(video["src"])
+
+# Display the pygame surface
+pygame.init()
+screen = pygame.display.set_mode((496, 496))
+screen.blit(surface, (0, 0))
+pygame.display.flip()
+
+# Wait for the user to close the window
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    surface = pygame.surfarray.make_surface(video["src"])
+    screen.blit(surface, (0, 0))
+    pygame.display.flip()
+
+pygame.quit()
