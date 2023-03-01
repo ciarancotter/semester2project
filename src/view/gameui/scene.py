d77c1fb015c4342b6877f438fea03037f835acee
Bradley Harris <bradx135@gmail.com>
2023-03-01 13:57:32 +0000

    about implemented

--- src/view/gameui/scene.py
+++ src/view/gameui/scene.py
@@ -297,18 +297,160 @@ class AboutMenuScene(Scene):
         self.label = GameState.about
         self.buttons = []
 
+        self.background_image = pygame.image.load("src/view/assets/aboutBG.png")
+        self.background_rect = self.background_image.get_rect()
+
+        self.logo = pygame.image.load("src/view/assets/logo.png")
+        self.logo = pygame.transform.scale(self.logo, (800, 150))
+
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.gold = (255, 215, 0)
+
+        self.bradley = pygame.image.load("src/view/assets/profiles/bradleyAbout.png")
+        self.niamh = pygame.image.load("src/view/assets/profiles/niamhAbout.png")
+        self.ciaran = pygame.image.load("src/view/assets/profiles/ciaranAbout.png")
+        self.samina = pygame.image.load("src/view/assets/profiles/saminaAbout.png")
+        self.shaza = pygame.image.load("src/view/assets/profiles/shazaAbout.png")
+        self.patrick = pygame.image.load("src/view/assets/profiles/patrickAbout.png")
+
+        self.bradley_description="Bradley Harris"
+        self.samina_description="Samina Arshad"
+        self.niamh_description="Niamh Connolly"
+        self.shaza_description="Shaza"
+        self.ciaran_description="Ciaran Cotter"
+        self.patrick_description="Patrick Lenhian"
+
+        self.bradley_role="Machine Vision Developer"
+        self.samina_role="Developer and Artist"
+        self.niamh_role="Pygame expert"
+        self.shaza_role="Pygame Developer"
+        self.ciaran_role="Ai and View specialist"
+        self.patrick_role="System Architect"
+
     def _render(self):
         '''Renders the about menu
         '''
-        self.background_image = pygame.image.load("src/view/assets/aboutBG.png")
-        about_text = pygame.font.SysFont("monospace", 30).render('About', True, "black")
-        about_text_rect = about_text.get_rect()
-        about_text_rect.center = (self.screen.get_width() // 2, (self.screen.get_height() // 2) - 50)
+        self.screen.blit(self.background_image, self.background_rect)
 
-        self.text = about_text
-        self.text_rect = about_text_rect
+        # Draw a gold square with rounded corners under the developers
+        square_position = (0, 190)
+        square_size = (700, 500)
+        border_radius = 20
+        pygame.draw.rect(self.screen, (self.gold), (square_position, square_size), border_radius=border_radius)
 
-        self.screen.blit(about_text, about_text_rect)
+        # Add text to the gold square
+        font = pygame.font.SysFont("monospace", 72, bold=True)
+        text = "MEET THE TEAM"
+        text_surface = font.render(text, True, self.black, self.gold)
+        text_rect = text_surface.get_rect()
+        text_rect.center = ((square_position[0] + square_size[0]) // 2, square_position[1] + 50)
+        self.screen.blit(text_surface, text_rect)
+
+        # Draw a black border around the square
+        border_position = (square_position[0] - 5, square_position[1] - 5)
+        border_size = (square_size[0] + 10, square_size[1] + 10)
+        border_radius = 20
+        pygame.draw.rect(self.screen, (self.black), (border_position, border_size), 5, border_radius=border_radius)
+
+        # Define the position and size of the text box
+        text_box_position = (800, 190)
+        text_box_size = (410, 300) 
+
+
+        # Define the font and color for the text
+        font = pygame.font.SysFont("monospace", 18, bold=True)
+        smallfont = pygame.font.SysFont("monospace", 14, bold=True)
+
+        # Create a surface with the desired text
+        long_sentence = "Welcome to Boole Raider! This is a camera recognition game which allows you to lead the character in the game and go through the levels with your own movements! We are a group of computer scientist students that have created this game for their software engineering project at UCC in the college year of 2022-2023. Click HELP on the main menu to learn how to play!"
+        text_surface = font.render(long_sentence, True, self.black)
+
+        # Wrap the text in a box of 400px width
+        lines = []
+        while long_sentence:
+            i = 1
+            # Find the maximum number of characters that fit in the 400px width
+            while font.size(long_sentence[:i])[0] < 400 and i < len(long_sentence):
+                i += 1
+            # If the entire sentence fits within the 400px width, add it as a line
+            if i == len(long_sentence):
+                lines.append(long_sentence)
+                long_sentence = ""
+            else:
+                # Find the last space within the 400px width to split the line
+                if " " in long_sentence[:i]:
+                    i = long_sentence[:i].rfind(" ") + 1
+                # Add the line to the list
+                lines.append(long_sentence[:i])
+                long_sentence = long_sentence[i:]
+
+        # Create a surface for the text box with a gold background
+        text_box_surface = pygame.Surface(text_box_size)
+        text_box_surface.fill(self.gold)
+
+        # Blit the wrapped text surface onto the text box surface
+        y = 10
+        for line in lines:
+            text_surface = font.render(line, True, self.black, self.gold)
+            text_box_surface.blit(text_surface, (10, y))
+            y += text_surface.get_height() + 5
+
+        # Create a surface for the black border
+        border_surface = pygame.Surface((text_box_size[0] + 10, text_box_size[1] + 10))
+        border_surface.fill(self.black)
+
+        # Draw the gold text box on top of the border surface
+        border_surface.blit(text_box_surface, (5, 5))
+
+        # Draw the black border with rounded corners on top of the gold text box
+        border_rect = pygame.draw.rect(border_surface, self.black, (0, 0, text_box_size[0]+10, text_box_size[1]+10), 5, border_radius=10)
+
+        # Blit the border surface onto the main window surface
+        self.screen.blit(border_surface, text_box_position)
+
+        # draw item images and descriptions
+        font = pygame.font.SysFont("monospace", 24, bold=True)
+        self.screen.blit(self.bradley, (0, 280))
+        bradley_text = font.render(self.bradley_description, True, (0,0,0))
+        self.screen.blit(bradley_text, (120, 350))
+        bradley_text2 = smallfont.render(self.bradley_role, True, (0,0,0))
+        self.screen.blit(bradley_text2, (120, 380))
+
+        self.screen.blit(self.niamh, (0, 420))
+        niamh_text = font.render(self.niamh_description, True, (0,0,0))
+        self.screen.blit(niamh_text, (120, 470))
+        niamh_text2 = smallfont.render(self.niamh_role, True, (0,0,0))
+        self.screen.blit(niamh_text2, (120, 500))
+
+        self.screen.blit(self.ciaran, (0, 540))
+        ciaran_text = font.render(self.ciaran_description, True, (0,0,0))
+        self.screen.blit(ciaran_text, (120, 600))
+        ciaran_text2 = smallfont.render(self.ciaran_role, True, (0,0,0))
+        self.screen.blit(ciaran_text2, (120, 630))
+
+        self.screen.blit(self.samina, (320, 300))
+        samina_text = font.render(self.samina_description, True, (0,0,0))
+        self.screen.blit(samina_text, (450, 350))
+        samina_text2 = smallfont.render(self.samina_role, True, (0,0,0))
+        self.screen.blit(samina_text2, (450, 380))
+
+        self.screen.blit(self.shaza, (320, 420))
+        shaza_text = font.render(self.shaza_description, True, (0,0,0))
+        self.screen.blit(shaza_text, (450, 470))
+        shaza_text2 = smallfont.render(self.shaza_role, True, (0,0,0))
+        self.screen.blit(shaza_text2, (450, 500))
+
+        self.screen.blit(self.patrick, (320, 540))
+        patrick_text = font.render(self.patrick_description, True, (0,0,0))
+        self.screen.blit(patrick_text, (450, 600))
+        patrick_text2 = smallfont.render(self.patrick_role, True, (0,0,0))
+        self.screen.blit(patrick_text2, (450, 630))
+        
+
+        logo_x_pos = (self.screen.get_width() - self.logo.get_width()) // 2
+
+        self.screen.blit(self.logo, (logo_x_pos, 20))
 
     def initialise(self):
         """Initialises some values of the About menu, but not immediately when the instance is created.
